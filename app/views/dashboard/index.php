9c18879f1d63b2443fbfba63216c53e95f931605
isabellyccamargo <camargoisa114@gmail.com>
2025-11-07 11:04:29 -0300

    Ajustando tamanho dashboard

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -1,10 +1,9 @@
 
-<div class="metabase-dashboard-container">
+<div class="metabase-dashboard-container ">
     <iframe
         src="http://localhost:3000/public/dashboard/d4805600-6e35-4fb0-b61a-92f1bc027ac1"
         frameborder="0"
         width="100%"
-        height="1500" 
-        allowtransparency
-    ></iframe>
+        height="810px"
+        allowtransparency></iframe>
 </div>
